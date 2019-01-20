9d70cd406989e0974fc3b0b854997d58d4e0a887
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2019-01-20 14:32:33 -0300

    Updated the README.md file with "how to run the test" option

--- README.md
+++ README.md
@@ -27,6 +27,27 @@ All of them have been taken care of.
 
 Incase of discrepancy please raise an issue or feel free to send pull requests.
 
+## Running the tests
+
+To run the tests, run the appropriate command below ([why they are different](https://github.com/pytest-de
+v/pytest/issues/1629#issue-161422224)):
+
+- Python 2.7: `py.test bikeRental_test.py`
+- Python 3.4+: `pytest bikeRental_test.py`
+
+Alternatively, you can tell Python to run the pytest module (allowing the same command to be used regardle
+ss of Python version):
+`python -m pytest bikeRental_test.py`
+
+### Common `pytest` options
+
+- `-v` : enable verbose output
+- `-x` : stop running tests on first failure
+- `--ff` : run failures from previous test before running other test cases
+
+For other options, see `python -m pytest -h`
+
+
 ### License
 
 This code is open source. So feel free to use, modify, share, download as per your need. I do not take risk nor responsibility for your errors or any commercial damage.
--- bikeRental.py
+++ bikeRental.py
@@ -7,12 +7,9 @@ A company rents bikes under following options:
 2. Rental by day, charging $20 a day
 3. Rental by week, changing $60 a week
 4. Family Rental, is a promotion that can include from 3 to 5 Rentals (of any type) with a discount of 30% of the total price
-
-# Assigment:
-1. Implement a set of classes to model this domain and logic
 2. Add automated tests to ensure a coverage over 85%
-3. Use GitHub to store and version your code
-4. Apply all the recommended practices you would use in a real project
+
+
 5. Add a README.md file to the root of your repository to explain: your design, the development practices you applied and how run the tests.
 
 Note: we don't expect any kind of application, just a set of classes with its automated tests.
@@ -143,7 +140,7 @@ class BikeRental:
             if (2 <= numOfBikes <= 5):
                 print("You are eligible for Family rental promotion of 30% discount")
                 bill = bill * 0.7
-                
+
             print("Thanks for returning your bike. Hope you enjoyed our service!")
             print("That would be ${}".format(bill))
             return bill
